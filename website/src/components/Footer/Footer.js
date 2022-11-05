907fbf81e7ae99d8e252ce84f44886fe96ccbeab
User <fuadfahrezy216@gmail.com>
2022-11-05 16:40:15 +0700

    update footbar

--- website/src/components/Footer/Footer.js
+++ website/src/components/Footer/Footer.js
@@ -13,8 +13,8 @@ const Footer = () => {
                         <h3>MY WEBSITE</h3>
                         <ul>
                             <li><Link to="/">Home</Link></li>
-                            <li><Link to="/about">About</Link></li>
                             <li><Link to="/contact">Contact</Link></li>
+                            <li><Link to="/list">Place</Link></li>
                         </ul>
                     </div>
                     <div class="col-md-2 item2">
